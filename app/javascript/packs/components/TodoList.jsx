0de5fd29bd0f0c10f724d64b95fb08e357d288f4
FumiyaShibusawa <shibusawa@basicinc.jp>
2018-05-20 23:33:50 +0900

    feat: Todoリスト／Todoを追加するフォームを作成

--- app/javascript/packs/components/TodoList.jsx
+++ app/javascript/packs/components/TodoList.jsx
@@ -1,19 +1,36 @@
 import * as React from "react"
 
 class TodoList extends React.Component {
+  toggleTodoListForm = () => {
+    $("#todo_list_form").toggle();
+  }
   render(){
     return (
       <div className="todolist_component">
         <ul>
-          <li>
-            <div>{ this.props.status }</div>
-            { this.props.todolist_name }
-          </li>
+            { this.props.todo_lists.map( (todo_list, i) => (
+              <li key={ `${todo_list.name}_${i}` }>{ todo_list.name }</li>
+            ) ) }
         </ul>
+        <div onClick={ this.toggleTodoListForm } >
+          <i className="fa fa-plus"></i>
+          <span>Todoリストを追加する</span>
+        </div>
+        <div id="todo_list_form" style={{ display: 'none' }}>
+          <input type="text"/>
+        </div>
       </div>
     )
   }
 }
 
+TodoList.defaultProps = {
+  todo_lists: [
+    {name: "TodoList1だよ"},
+    {name: "TodoList2だよ"},
+    {name: "TodoList3だよ"},
+    {name: "TodoList4だよ"},
+  ]
+}
 
 export default TodoList
