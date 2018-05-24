e74443a007c2e1f87d6e90ecf339c32802e67f69
FumiyaShibusawa <shibusawa@basicinc.jp>
2018-05-25 02:35:21 +0900

    update: stateの初期値を単なる空のオブジェクトに変更

--- app/javascript/packs/reducers/todolistReducer.js
+++ app/javascript/packs/reducers/todolistReducer.js
@@ -1,12 +1,4 @@
-// const initialState = {
-//   todo_lists: [
-//   {name: "TodoList1だよ"},
-//   {name: "TodoList2だよ"},
-//   {name: "TodoList3だよ"},
-//   {name: "TodoList4だよ"},
-// ]}
-
-const todolists = (state = {todo_lists: []}, action) => {
+const todolists = (state = { todo_lists: [] }, action) => {
   switch (action.type) {
     case "ADD_TODOLIST":
       return { todo_lists: [
