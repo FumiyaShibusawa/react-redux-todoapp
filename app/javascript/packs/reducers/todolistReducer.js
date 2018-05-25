f6d5a32c9f5eb18aae77a879bc53e7d9039ad30e
FumiyaShibusawa <shibusawa@basicinc.jp>
2018-05-26 03:41:49 +0900

    update: reducerを一つに集約

--- app/javascript/packs/reducers/todolistReducer.js
+++ app/javascript/packs/reducers/todolistReducer.js
@@ -2,9 +2,18 @@ const todolists = (state = { todo_lists: [] }, action) => {
   switch (action.type) {
     case "ADD_TODOLIST":
       return { todo_lists: [
-          ...state.todo_lists,
-          { name: action.text }
-        ] }
+        ...state.todo_lists,
+        { name: action.todo_list, todos: [] }
+      ] }
+    case "ADD_TODO":
+      for (let i = 0; i < state.todo_lists.length; i++) {
+        if (i === action.index) {
+          state.todo_lists[i].todos.push({ name: action.todo, status: "open" })
+        }
+      }
+      return { todo_lists: [
+        ...state.todo_lists
+      ] }
     default:
       return state
   }
