f3e0a3cadbb524fc3085705fff176a35511a6724
FumiyaShibusawa <shibusawa@basicinc.jp>
2018-05-26 03:47:04 +0900

    update: TodoListをクリックしたら詳細を表示するイベントを追加

--- app/javascript/packs/components/TodoList.jsx
+++ app/javascript/packs/components/TodoList.jsx
@@ -7,22 +7,24 @@ class TodoList extends React.Component {
       <div className="todolist_component">
         <ul>
             { this.props.todo_lists.map( (todo_list, i) => (
-              <li key={ `${todo_list.name}_${i}` }>{ todo_list.name }</li>
+              <li
+              key={ `${todo_list.name}_${i}` }
+              onClick={ () => { this.props.showTodos(i) } }
+              >{ todo_list.name }</li>
             ) ) }
         </ul>
-        <div onClick={ this.props.toggleTodoListForm } >
+        <div onClick={ this.props.toggleTodoListForm }>
           <i className="fa fa-plus"></i>
           <span>Todoリストを追加する</span>
         </div>
-        <div id="todo_list_form" style={{ display: 'none' }}>
-          <input type="text" ref= { node => { input = node }}/>
+        <div id="todo_list_form" style={ { display: 'none' } }>
+          <input type="text" ref= { node => { input = node } }/>
           <input type="submit" value="送信" onClick={ () => {
             if (input) {
               this.props.addNewTodoList(input.value);
               input.value = ""; // テキストボックス内の値をクリア
             }
-          }
-          }/>
+          } }/>
         </div>
       </div>
     )
