61df2f69d8a5ef5c26f54ccbdac52a2af5514704
FumiyaShibusawa <shibusawa@basicinc.jp>
2018-05-27 03:23:04 +0900

    feat: redux-thunkを追加

--- app/javascript/packs/stores/index.js
+++ app/javascript/packs/stores/index.js
@@ -1,13 +1,14 @@
 import * as React from "react"
 import { render } from "react-dom"
 import { Provider } from "react-redux"
-import { createStore } from "redux"
+import { createStore, applyMiddleware } from "redux"
+import thunkMiddleware from "redux-thunk"
 import rootReducer from "../reducers"
 import App from "../components/App"
 import { addTodoList } from "../actions/TodoListActions"
 
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer,applyMiddleware(thunkMiddleware))
 const todolist_root = document.getElementById("todolist-root")
 if (todolist_root) {
   render(
