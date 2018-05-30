3893bd87b38d04e1f762a8231ce39899ef11ccac
FumiyaShibusawa <shibusawa@basicinc.jp>
2018-05-30 23:49:23 +0900

    feat: Todo新規追加／削除のアクションを追加

--- app/javascript/packs/actions/TodoActions.js
+++ app/javascript/packs/actions/TodoActions.js
@@ -1,11 +1,62 @@
-export const addTodo = (todo, index) => ({
+export const addTodo = (todo, todo_list_id, index) => {
+  return dispatch => {
+    fetch("/todos.json", {
+      method: "POST",
+      body: JSON.stringify({ todo: { name: todo, todo_list_id: todo_list_id } }),
+      headers: new Headers({
+        "Content-Type": "application/json"
+      })
+    })
+    .then(response => response.json())
+    .then(
+      result => (dispatch(addTodoSuccess(result, todo_list_id, index))),
+      error => (dispatch(addTodoFailed(error, todo_list_id, index)))
+    )
+  }
+}
+
+const addTodoSuccess = (todo, todo_list_id, index) => ({
   type: "ADD_TODO",
   todo: todo,
+  todo_list_id: todo_list_id,
   index: index
 })
+const addTodoFailed = (todo, todo_list_id, index) => ({
+  type: "ADD_TODO_FAILED",
+  todo: todo,
+  todo_list_id: todo_list_id,
+  index: index
+})
+
 
 export const showTodos = (todo_list) => ({
   type: "SHOW_TODOS",
   todo_list_name: todo_list.name,
   todos: todo_list.todos
+})
+
+
+export const deleteTodo = (todo) => {
+  return dispatch => {
+    fetch(`/todos/${todo._id["$oid"]}.json`, {
+      method: "DELETE",
+      headers: new Headers({
+        "Content-Type": "application/json"
+      })
+    })
+    .then(response => response.json())
+    .then(
+      result => (dispatch(deleteTodoSuccess(result))),
+      error => (dispatch(deleteTodoFailed(error)))
+    )
+  }
+}
+
+const deleteTodoSuccess = (todo) => ({
+  type: "DELETE_TODO",
+  todo: todo
+})
+const deleteTodoFailed = (todo) => ({
+  type: "DELETE_TODO_FAILED",
+  todo: todo
 })
