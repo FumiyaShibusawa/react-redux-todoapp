5d3dee9502075e6349315158052def9a082dda62
FumiyaShibusawa <shibusawa@basicinc.jp>
2018-06-08 17:18:31 +0900

    feat: 一旦Userモデルをcreateするアクションまで作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+    if user.save
+      todo_list_data = TodoList.where(user_id: user.id).to_json(include: :todos)
+      respond_to do |format|
+        format.html
+        format.json { render json: todo_list_data }
+      end
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
 end
--- app/javascript/packs/components/NonAuth/Signup.jsx
+++ app/javascript/packs/components/NonAuth/Signup.jsx
@@ -3,8 +3,28 @@ import { Field, reduxForm } from "redux-form"
 import { BrowserRouter as Router, Link } from "react-router-dom"
 
 class Signup extends React.Component {
-  handleSubmit = (values) => {
-    console.log(values)
+  handleSubmit = (e) => {
+    let email, password;
+    for (let i = 0; i < e.target.children.length; i++) {
+      if (e.target.children[i].name == "email") {
+        email = e.target.children[i].value;
+      }
+      if (e.target.children[i].name == "password") {
+        password = e.target.children[i].value;
+      }
+    }
+    fetch("/users.json", {
+      method: "POST",
+      body: JSON.stringify({ user: { email: email, password: password } }),
+      headers: new Headers({
+        "Content-Type": "application/json"
+      })
+    })
+    .then(response => response.json())
+    .then(
+      result => (console.log(result)),
+      error => (console.log(error))
+    )
   }
 
   render() {
@@ -14,7 +34,7 @@ class Signup extends React.Component {
           <h1>Register</h1>
           <form onSubmit={ (e) => {
             e.preventDefault();
-            this.handleSubmit(e.target);
+            this.handleSubmit(e);
           } }>
             <Field
             name="email"
@@ -25,7 +45,7 @@ class Signup extends React.Component {
             <Field
             name="password"
             component="input"
-            type="text"
+            type="password"
             placeholder="Enter your password"
             />
             <button type="submit">Register</button>
