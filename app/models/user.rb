ed6a581880f2338c861057812df1f13083e559c7
FumiyaShibusawa <shibusawa@basicinc.jp>
2018-06-12 23:12:58 +0900

    update: bcryptを使ってパスワードをハッシュ化

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,23 @@
 class User
   include Mongoid::Document
   include Mongoid::Timestamps
+  include BCrypt
 
   has_many :todo_lists
 
   field :name, type: String
   field :email, type: String
-  field :password, type: String
+  field :password_hash, type: String
 
-  validate :name, :email, presence: true
-  validate :name, :email, uniqueness: true
+  validates :name, :email, presence: true
+  validates :name, :email, uniqueness: true
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
 end
