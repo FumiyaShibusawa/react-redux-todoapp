2a4937975c039483fb91b9472dbab0f6bca67d22
FumiyaShibusawa <shibusawa@basicinc.jp>
2018-06-13 15:43:01 +0900

    feat: ログイン後のページにアクセスするときはjwt認証を通す実装を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery unless: -> { request.format.json? }
+  before_action :authenticate
+
+  require "auth"
+
+  def authenticate
+    redirect_to root_path, notice: "not authorized" unless current_user
+  end
+
+  def current_user
+    if jwt_token.present?
+      decoded_token = Auth.decode(jwt_token)
+      @current_user = User.where(_id: decoded_token[0]["_id"]).first
+    end
+  end
+  def jwt_token
+    return nil if request.headers["Authentication"].nil?
+    request.headers["Authentication"].split(" ").last
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,13 @@
 class SessionsController < ApplicationController
+  skip_before_action :authenticate, only: [:index]
+
   def index
   end
 
   def create
     # TODO: JWT認証で発行したトークンをdecodeして取得したuser_idで検索するようにする
-    @user = User.find_by(email: session_params[:email])
-    if @user
+    decoded_token = Auth.decode(jwt_token)
+    if current_user.password == decoded_token[0]["password"]
       respond_to do |format|
         format.html { redirect_to todo_lists_path }
       end
@@ -18,6 +20,6 @@ class SessionsController < ApplicationController
 
   private
   def session_params
-    params.require(:session).permit(:email, :password)
+    params.require(:session).permit(:name, :email, :password)
   end
 end
