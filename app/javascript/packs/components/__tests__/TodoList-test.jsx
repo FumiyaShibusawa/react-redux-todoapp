da7ec00256fce5c6e0e3c5d93d1b7ffdcf6d1d28
FumiyaShibusawa <shizai.asdfghjkl@gmail.com>
2018-07-15 23:24:00 +0900

    update: wrapped test suites with describe

--- app/javascript/packs/components/__tests__/TodoList-test.jsx
+++ app/javascript/packs/components/__tests__/TodoList-test.jsx
@@ -1,22 +1,32 @@
 import * as React from 'react';
 import TodoList from '../TodoList';
 import renderer from 'react-test-renderer';
+import ReactTestUtils from 'react-dom/test-utils';
 
-test('it renders correctly', () => {
-  const todo_lists = [
-    {
-      _id: { $oid: "5b2d02e7f450fe5251b9eb9d" },
-      user_id: { $oid: "5b2cf7fef450fe50d9d60871" },
-      created_at: "2018-06-22T14:08:39.983Z",
-      updated_at: "2018-06-22T14:08:39.983Z",
-      name: "write code everyday",
-      todos: []
-    }
-  ]
-  const tree = renderer.create(
-    <TodoList
+const todo_lists = [
+  {
+    _id: { $oid: "5b2d02e7f450fe5251b9eb9d" },
+    user_id: { $oid: "5b2cf7fef450fe50d9d60871" },
+    created_at: "2018-06-22T14:08:39.983Z",
+    updated_at: "2018-06-22T14:08:39.983Z",
+    name: "write code everyday",
+    todos: []
+  }
+]
+describe('<TodoList />', () => {
+  test('should render without error', () => {
+    const tree = renderer.create(
+      <TodoList
+        todo_lists={todo_lists}
+      />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  test('is a valid React Element', () => {
+    const isValidElement = ReactTestUtils.isElement(<TodoList
       todo_lists={todo_lists}
-    />
-  ).toJSON();
-  expect(tree).toMatchSnapshot();
+    />);
+    expect(isValidElement).toBe(true);
+  });
+
 });
