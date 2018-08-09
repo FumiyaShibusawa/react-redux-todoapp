e052327d7e381c25538b8d39425984ebc138a0eb
FumiyaShibusawa <shizai.asdfghjkl@gmail.com>
2018-08-09 23:55:28 +0900

    update: added test for toggleForm

--- app/javascript/packs/components/__tests__/Todo-test.jsx
+++ app/javascript/packs/components/__tests__/Todo-test.jsx
@@ -1,30 +1,40 @@
 import * as React from 'react';
 import Todo from '../Todo';
+import { Form } from '../TodoList';
 import renderer from 'react-test-renderer';
+import { shallow, mount, render } from 'enzyme';
 
-test('it renders correctly', () => {
-  const todo_lists = [
-    {
-      _id: { $oid: "5b2d02e7f450fe5251b9eb9d" },
-      user_id: { $oid: "5b2cf7fef450fe50d9d60871" },
+const todo_lists = [
+  {
+    _id: { $oid: "5b2d02e7f450fe5251b9eb9d" },
+    user_id: { $oid: "5b2cf7fef450fe50d9d60871" },
+    created_at: "2018-06-22T14:08:39.983Z",
+    updated_at: "2018-06-22T14:08:39.983Z",
+    name: "write code everyday",
+    todos: [{
+      _id: { $oid: "5b237ade2208a2001bc74882" },
       created_at: "2018-06-22T14:08:39.983Z",
       updated_at: "2018-06-22T14:08:39.983Z",
-      name: "write code everyday",
-      todos: [{
-        _id: { $oid: "5b237ade2208a2001bc74882" },
-        created_at: "2018-06-22T14:08:39.983Z",
-        updated_at: "2018-06-22T14:08:39.983Z",
-        name: "open your editor",
-        todo_list_id: { $oid: "5b2d02e7f450fe5251b9eb9d" },
-        completed: false
-      }]
-    }
-  ]
-  const tree = renderer.create(
-    <Todo
-      todo_lists={todo_lists[0]}
-      num={0}
-    />
-  ).toJSON();
-  expect(tree).toMatchSnapshot();
+      name: "open your editor",
+      todo_list_id: { $oid: "5b2d02e7f450fe5251b9eb9d" },
+      completed: false
+    }]
+  }
+]
+describe('<Todo />', () => {
+  test('it renders correctly', () => {
+    const tree = renderer.create(
+      <Todo
+        todo_list={todo_lists[0]}
+        num={0}
+      />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  })
+  // toggleForm
+  test('state "isFormToggled" set to true when toggleForm fired', () => {
+    const todo = mount(<Todo todo_list={todo_lists[0]} num={0} />);
+    todo.find('.add-button-text').simulate('click');
+    expect(todo.state('isFormToggled')).toBe(true);
+  });
 });
